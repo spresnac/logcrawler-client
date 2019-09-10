0f8d1d7b19843bba9f4ea4b50413b631d9dd43ab
Sascha <presnac@gmail.com>
2019-09-10 21:27:30 +0200

    - fine tune the service provider

--- src/spresnac/logcrawlerclient/Providers/LogcrawlerClientServiceProvider.php
+++ src/spresnac/logcrawlerclient/Providers/LogcrawlerClientServiceProvider.php
@@ -3,23 +3,18 @@
 namespace spresnac\logcrawlerclient\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use spresnac\logcrawlerclient\Handler\LogCrawler;
 
 class LogcrawlerClientServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/logcrawler.php' => base_path('config/logcrawler.php'),
+            __DIR__.'/../Config/logcrawler.php' => base_path('config/logcrawler.php'),
         ], 'logcrawlerclient-config');
     }
 
     public function register()
     {
-        $this->app->bind('logcrawlerclient', function() {
-            return new LogCrawler();
-        });
-
-        $this->mergeConfigFrom(__DIR__.'/../config/logcrawler.php', 'logcrawler');
+        $this->mergeConfigFrom(__DIR__.'/../Config/logcrawler.php', 'logcrawler');
     }
 }
